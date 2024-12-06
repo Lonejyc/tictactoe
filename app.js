fbbb47485996164bf4d7ea8ccc6d7d26d957e2ee
Lonejyc <jocelyn.marcilloux@gmail.com>
2024-12-06 13:30:47 +0100

    chore: add comments

--- app.js
+++ app.js
@@ -14,37 +14,23 @@ let arr = [];
 let playingArray = [];
 
 io.on('connection', (socket) => {
+     // Événement 'find' pour ajouter des joueurs et démarrer une partie
      socket.on('find', (e) => {
           if (e.name != null) {
                arr.push(e.name);
                if (arr.length >= 2) {
-                    let p1obj = {
-                         p1name: arr[0],
-                         p1value: 'X',
-                         p1move: ""
-                    }
-
-                    let p2obj = {
-                         p2name: arr[1],
-                         p2value: 'O',
-                         p2move: ""
-                    }
-
-                    let obj = {
-                         p1: p1obj,
-                         p2: p2obj,
-                         sum: 1,
-                         turn: 'X' // Ajout de la propriété turn
-                    }
+                    // Création des objets joueurs et initialisation de la partie
+                    let p1obj = { p1name: arr[0], p1value: 'X', p1move: "" }
+                    let p2obj = { p2name: arr[1], p2value: 'O', p2move: "" }
+                    let obj = { p1: p1obj, p2: p2obj, sum: 1, turn: 'X' }
                     playingArray.push(obj);
-
                     arr.splice(0, 2);
-
                     io.emit('find', { allPlayers: playingArray });
                }
           }
      });
 
+     // Événement 'playing' pour gérer les mouvements des joueurs
      socket.on('playing', (e) => {
           console.log('Received playing event:', e);
           let objToChange;
@@ -53,14 +39,14 @@ io.on('connection', (socket) => {
                if (objToChange && objToChange.turn === 'X') {
                     objToChange.p1.p1move = e.id;
                     objToChange.sum++;
-                    objToChange.turn = 'O'; // Changer le tour
+                    objToChange.turn = 'O';
                }
           } else if (e.value == 'O') {
                objToChange = playingArray.find(obj => obj.p2.p2name === e.name);
                if (objToChange && objToChange.turn === 'O') {
                     objToChange.p2.p2move = e.id;
                     objToChange.sum++;
-                    objToChange.turn = 'X'; // Changer le tour
+                    objToChange.turn = 'X';
                }
           }
 
@@ -71,19 +57,23 @@ io.on('connection', (socket) => {
           }
      });
 
+     // Événement 'gameOver' pour supprimer un joueur après la fin de la partie
      socket.on("gameOver", (e) => {
           playingArray = playingArray.filter(obj => obj.p1.p1name !== e.name);
      });
 
+     // Événement 'chatMessage' pour gérer les messages de chat
      socket.on('chatMessage', (msg) => {
           io.emit('chatMessage', msg);
      });
 });
 
+// Route pour servir le fichier index.html
 app.get('/', (req, res) => {
      return res.sendFile("index.html", { root: __dirname });
 });
 
+// Démarrage du serveur sur le port 3000
 server.listen(3000, () => {
      console.log('listening on port 3000');
 });
